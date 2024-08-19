e4d56190228053ee7f74694a549c64716ad3707e
Karan Singh Dhir <ksinghdhir1@gmail.com>
2024-08-18 18:54:23 -0700

    add: btn and heading

--- src/components/common/ArrowBtn.jsx
+++ src/components/common/ArrowBtn.jsx
@@ -3,10 +3,11 @@ import { IoIosArrowForward, IoIosArrowBack } from 'react-icons/io'
 const ArrowBtn = ({ onClick, direction, variant }) => {
   const variants = {
     one: 'w-5 h-5 text-secondary group-hover:text-background',
-    two: 'w-12 h-12 text-primary',
+    two: 'w-5 h-5 text-primary group-hover:text-secondary',
   }
   const btnVariants = {
     one: 'p-2 border-2 border-secondary rounded-full group hover:bg-secondary',
+    two: 'p-2 border-2 border-primary rounded-full group hover:bg-primary',
   }
   return (
     <button className={btnVariants[variant] ? btnVariants[variant] : ''}>
--- src/components/menu/HeroSecondary.jsx
+++ src/components/menu/HeroSecondary.jsx
@@ -1,7 +1,33 @@
 import React from 'react'
+import ArrowBtn from '@/components/common/ArrowBtn'
 
 const HeroSecondary = () => {
-  return <div className="p-8 max-sm:p-4">Hero Secondary</div>
+  return (
+    <div className="p-8 max-sm:p-4">
+      {/* secondary header */}
+      <div className="flex flex-row justify-between">
+        <h2 className="text-2xl font-bold bg-primary text-secondary p-2 rounded-lg">
+          Featured
+        </h2>
+
+        <div className="self-center flex flex-row gap-2">
+          <ArrowBtn variant="two" onClick={() => swiperRef.current.slidePrev()}>
+            Left
+          </ArrowBtn>
+          <ArrowBtn
+            variant="two"
+            direction={'right'}
+            onClick={() => swiperRef.current.slideNext()}
+          >
+            Right
+          </ArrowBtn>
+        </div>
+      </div>
+
+      {/* swiper component */}
+      <div></div>
+    </div>
+  )
 }
 
 export default HeroSecondary
